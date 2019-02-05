ccb87e9596c903a6e9c5b070bc50a6e9425d7648
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-02-05 11:31:03 -0700

    style top word and provide alert to use

--- src/index.js
+++ src/index.js
@@ -23,31 +23,33 @@ $(document).ready(() => {
   fetch(`http://localhost:3000/api/v1/top_word`)
   .then(response => response.json())
   .then(data => {
-    localStorage.setItem("topWord", JSON.stringify(data.word));
-    let topWord = JSON.parse(localStorage.getItem("topWord"));
+    let topWord = data.word;
+    let word = Object.keys(topWord);
+    let count = Object.values(topWord);
+
+    let wordCount = document.querySelector('.word-count');
+    let topWordField = document.querySelector('#word');
+
+    topWordField.innerHTML += word;
+
+    wordCount.innerHTML = `<p>${count}</p>`;
   })
   .catch(error => console.error(error))
 
-  let wordCount = document.querySelector('.word-count');
-  let topWord = JSON.parse(localStorage.getItem("topWord"));
 
-  let word = Object.keys(topWord);
-  console.log(word);
-
-  let count = Object.values(topWord);
-  console.log(count);
-
-  wordCount.innerHTML = `${word}: ${count}`;
 
   let breakDown = document.getElementById('break-down');
   breakDown.addEventListener('click', function (e) {
     let textArea = document.querySelector('textarea');
-    let words = textArea.value.split(" ");
-    console.log(words);
+
+    let noNewLine = textArea.value.replace(/\n/g, " ");
+    let words = noNewLine.split(" ");
 
     for (var i= 0; i< words.length; i++){
       postWords(words[i]);
     }
+
+    alert("Your string has been added to database!");
 
     //prevent the normal submission of the form
     e.preventDefault();
