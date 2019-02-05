5520f7a9c0c61bb2421f38f51ffc16f31de356f3
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-02-05 10:28:00 -0700

    Create functionality to post multiple words to database

--- src/index.js
+++ src/index.js
@@ -1,30 +1,22 @@
 import $ from 'jquery';
 
-function getWord(){
-  //if (localStorage.getItem("storedForecast") == null)
+
+
+
+
+$(document).ready(() => {
+  // have fun!
+
   fetch(`http://localhost:3000/api/v1/top_word`)
   .then(response => response.json())
   .then(data => {
     localStorage.setItem("topWord", JSON.stringify(data.word));
     let topWord = JSON.parse(localStorage.getItem("topWord"));
-    console.log(topWord);
   })
   .catch(error => console.error(error))
-}
-
-$(document).ready(() => {
-  // have fun!
-  console.log("hello?");
-  let breakDownButton = document.querySelector('button');
-  breakDownButton.addEventListener('click', function(){
-    let textArea = document.querySelector('textarea');
-    let value = textArea.value;
-    console.log(value);
-  });
 
   let wordCount = document.querySelector('.word-count');
   let topWord = JSON.parse(localStorage.getItem("topWord"));
-  console.log(topWord);
 
   let word = Object.keys(topWord);
   console.log(word);
@@ -34,4 +26,26 @@ $(document).ready(() => {
 
   wordCount.innerHTML = `${word}: ${count}`;
 
+  let breakDown = document.getElementById('break-down');
+  breakDown.addEventListener('click', function (e) {
+    let textArea = document.querySelector('textarea');
+    let words = textArea.value.split(" ");
+    console.log(words);
+
+    for (var i= 0; i< words.length; i++){
+
+    let addWordsUrl = "http://localhost:3000/api/v1/words"
+    let addWordsData = {
+      word: { value: words[i] }
+    }
+
+    $.post(addWordsUrl, addWordsData, function(data, status){
+        console.log(data);
+    });
+    }
+
+    //prevent the normal submission of the form
+    e.preventDefault();
+
+  });
 })
